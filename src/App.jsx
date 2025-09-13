6385d0df759895d8871d7c189b364b7e1dc98409
M3hTi <mehdinasir502@yahoo.com>
2025-09-13 10:30:48 +0330

    🚀 feat: integrate React Query and Devtools
    - Wrap app with `QueryClientProvider`
    - Add `ReactQueryDevtools` for debugging
    - Initialize and provide `QueryClient` instance

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./features/authentication/Login";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
